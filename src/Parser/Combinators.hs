155134a2fae853d4cc0b75e2e4c6c9cfaac2bd87
Felix Springer <felixspringer149@gmail.com>
2022-02-20 21:57:00 +0100

    Add some simple parser combinators.

---
+++ src/Parser/Combinators.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RebindableSyntax #-}
+
+module Parser.Combinators where
+
+import qualified Data.List.NonEmpty
+import Parser
+import qualified Prelude
+
+many :: Prelude.Monad m => ParserT 'Prelude.True t e m a -> ParserT 'Prelude.False t e m [a]
+many p = map Data.List.NonEmpty.toList (some p) <|> pure []
+
+some :: Prelude.Monad m => ParserT 'Prelude.True t e m a -> ParserT 'Prelude.True t e m (Data.List.NonEmpty.NonEmpty a)
+some p = do
+    x <- p
+    xs <- many p
+    pure (x Data.List.NonEmpty.:| xs)
