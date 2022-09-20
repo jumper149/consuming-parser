5a766927aaf70e8eeb69630119c0c4e665b6a1a8
Felix Springer <felixspringer149@gmail.com>
2022-09-20 22:31:08 +0200

    Simplify some terms.

--- src/Parser/Example/Parens.hs
+++ src/Parser/Example/Parens.hs
@@ -15,44 +15,42 @@ openingParser = P.do
     x <- P.consume
     case x of
       '(' -> P.pure ()
-      _ -> P.throw (P.ErrorCustom "No opening parens.")
+      _ -> P.throw $ P.ErrorCustom "No opening parens."
 
 closingParser :: Monad m => P.ParserT 'P.Consuming Char String m ()
 closingParser = P.do
     x <- P.consume
     case x of
       ')' -> P.pure ()
-      _ -> P.throw (P.ErrorCustom "No closing parens.")
+      _ -> P.throw $ P.ErrorCustom "No closing parens."
 
 data Tree = Node
           | Fingers (NonEmpty.NonEmpty Tree)
   deriving Show
 
 nodeParser :: Monad m => P.ParserT 'P.Consuming Char String m Tree
-nodeParser = throwOn (P.ErrorCustom "No node.")
-  (P.do
+nodeParser =
+  throwOn (P.ErrorCustom "No node.") $ P.do
     openingParser
     closingParser
     P.pure Node
-  )
 
 fingersParser :: Monad m => P.ParserT 'P.Consuming Char String m Tree
-fingersParser = throwOn (P.ErrorCustom "No fingers.")
-  (P.do
+fingersParser =
+  throwOn (P.ErrorCustom "No fingers.") $ P.do
     openingParser
     insideResult <- P.some treeParser
     closingParser
     P.pure (Fingers insideResult)
-  )
 
 treeParser :: Monad m => P.ParserT 'P.Consuming Char String m Tree
 treeParser = throwOn (P.ErrorCustom "No tree.") (nodeParser P.<|> fingersParser)
 
 fullParser :: Monad m => P.ParserT 'P.Consuming Char String m Tree
 fullParser = P.do
-    result <- treeParser
-    P.end P.<|> P.throw (P.ErrorCustom "Additional input detected.")
-    P.pure result
+  result <- treeParser
+  P.end P.<|> P.throw (P.ErrorCustom "Additional input detected.")
+  P.pure result
 
 drawTree :: Tree -> String
 drawTree tree = Containers.drawTree (convertTree tree)
