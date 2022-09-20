dc23fb6b0a9ce5abd220d2557334b7d760f4a397
Felix Springer <felixspringer149@gmail.com>
2022-09-20 17:58:22 +0200

    Add `optional`.

--- src/Parser/Combinators.hs
+++ src/Parser/Combinators.hs
@@ -7,6 +7,9 @@ import qualified Data.List.NonEmpty
 import Parser
 import qualified Prelude
 
+optional :: Prelude.Monad m => ParserT 'Consuming t e m a -> ParserT 'Unknown t e m (Prelude.Maybe a)
+optional p = catch (Prelude.Just <$> p) (\ _ -> pure Prelude.Nothing)
+
 many :: Prelude.Monad m => ParserT 'Consuming t e m a -> ParserT 'Unknown t e m [a]
 many p = Data.List.NonEmpty.toList <$> some p <|> pure []
 
