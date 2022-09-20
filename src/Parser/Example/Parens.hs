97481b0729ed8f19dbf980c49b8a7d786aa848d7
Felix Springer <felixspringer149@gmail.com>
2022-09-20 14:35:13 +0200

    Add simple example.

---
+++ src/Parser/Example/Parens.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RebindableSyntax #-}
+module Parser.Example.Parens where
+
+import qualified Data.Tree as Containers
+import qualified Control.Monad.Trans.Class as T
+import qualified Control.Monad.Trans.State as T
+import qualified Data.List.NonEmpty as NonEmpty
+import Parser
+import Parser.Combinators
+import qualified Prelude
+import Data.String (fromString)
+
+data Nat = Z
+         | S Nat
+  deriving Prelude.Show
+
+data Err = ErrUnknownToken
+         | ErrUnmatchedClosingParens
+         | ErrUnmatchedOpeningParens
+  deriving Prelude.Show
+
+data Token = OpeningParens
+           | ClosingParens
+  deriving Prelude.Show
+parser :: ParserT 'Prelude.False Prelude.Char Err (T.State Nat) [Token]
+parser = do
+  result <- many tokenParser
+  count <- T.lift T.get
+  case count of
+    Z -> pure result
+    S _ -> throw (ErrorCustom ErrUnmatchedOpeningParens)
+
+tokenParser :: ParserT 'Prelude.True Prelude.Char Err (T.State Nat) Token
+tokenParser = do
+    x <- consume
+    case x of
+      '(' -> do
+          T.lift (T.modify S)
+          pure OpeningParens
+      ')' -> do
+          count <- T.lift T.get
+          case count of
+            Z -> throw (ErrorCustom ErrUnmatchedClosingParens)
+            S remainingCount -> do
+                T.lift (T.put remainingCount)
+                pure ClosingParens
+      _ -> throw (ErrorCustom ErrUnknownToken)
+
+----------------------------------------------------------------------------------------------------
+
+throwOn :: Prelude.Monad m => Error e -> ParserT 'Prelude.True t e m a -> ParserT 'Prelude.True t e m a
+throwOn e p = p <|> throw e
+
+openingParser :: Prelude.Monad m => ParserT 'Prelude.True Prelude.Char Prelude.String m ()
+openingParser = do
+    x <- consume
+    case x of
+      '(' -> pure ()
+      _ -> throw (ErrorCustom "No opening parens.")
+
+closingParser :: Prelude.Monad m => ParserT 'Prelude.True Prelude.Char Prelude.String m ()
+closingParser = do
+    x <- consume
+    case x of
+      ')' -> pure ()
+      _ -> throw (ErrorCustom "No closing parens.")
+
+data Tree = Node
+          | Fingers (NonEmpty.NonEmpty Tree)
+  deriving Prelude.Show
+
+nodeParser :: Prelude.Monad m => ParserT 'Prelude.True Prelude.Char Prelude.String m Tree
+nodeParser = throwOn (ErrorCustom "No node.")
+  (do
+    openingParser
+    closingParser
+    pure Node
+  )
+
+fingersParser :: Prelude.Monad m => ParserT 'Prelude.True Prelude.Char Prelude.String m Tree
+fingersParser = throwOn (ErrorCustom "No fingers.")
+  (do
+    openingParser
+    insideResult <- some treeParser
+    closingParser
+    pure (Fingers insideResult)
+  )
+
+treeParser :: Prelude.Monad m => ParserT 'Prelude.True Prelude.Char Prelude.String m Tree
+treeParser = throwOn (ErrorCustom "No tree.") (nodeParser <|> fingersParser)
+
+fullParser :: Prelude.Monad m => ParserT 'Prelude.True Prelude.Char Prelude.String m Tree
+fullParser = do
+    result <- treeParser
+    end <|> throw (ErrorCustom "Additional input detected.")
+    pure result
+
+drawTree :: Tree -> Prelude.String
+drawTree tree = Containers.drawTree (convertTree tree)
+ where
+  convertTree Node = Containers.Node "x" []
+  convertTree (Fingers (x NonEmpty.:| xs)) = Containers.Node "x" (convertTree Prelude.<$> (x : xs))
+
+
