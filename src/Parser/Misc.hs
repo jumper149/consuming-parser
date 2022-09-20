4419b272bca252371873a74795c0fbaa24d6fae4
Felix Springer <felixspringer149@gmail.com>
2022-09-20 23:01:23 +0200

    Add a simple `displayResult` function.

---
+++ src/Parser/Misc.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Parser.Misc where
+
+import Data.Failable
+import qualified Parser as P
+
+displayResult :: (Show a, Show e, Show t) => Failable (P.Error e) (a, [t]) -> String
+displayResult x =
+    case x of
+      Failed err -> "Failed to parse:\n" ++ displayError err
+      Succeeding (val, tokens) -> "Parsed value: " ++ show val ++ "\nRemaining Tokens: " ++ show tokens ++ "\n"
+
+displayError :: Show e => P.Error e -> String
+displayError = \case
+  P.ErrorCustom e -> show e ++ "\n"
+  P.ErrorInputEmpty -> "InputEmpty\n"
+  P.ErrorInputLeft -> "InputLeft\n"
+  P.ErrorAppend e1 e2 -> 
+    let str1 = displayError e1
+        str2 = displayError e2
+     in str2 ++ unlines (("  " ++) <$> lines str1)
+      
