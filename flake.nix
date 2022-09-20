f589165b5fc360d2c195c0ea71fdf1ccccb85a67
Felix Springer <felixspringer149@gmail.com>
2022-09-20 23:17:41 +0200

    Use GHC 9.2.4.

--- flake.nix
+++ flake.nix
@@ -12,21 +12,61 @@
 
   outputs = { self, nixpkgs }: {
 
+    overlays.default = final: prev: {
+      haskellPackages = prev.haskell.packages.ghc924.extend (haskellFinal: haskellPrev: { # TODO: Using GHC 9.2.4.
+      });
+    };
+
     defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      let
-        src = nix-gitignore.gitignoreSource [] ./.;
+      with import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default ]; };
+      let src = nix-gitignore.gitignoreSource [] ./.;
       in haskellPackages.callCabal2nix "consuming-parser" src {};
 
+    checks.x86_64-linux.fourmolu =
+      with import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default ]; };
+      stdenv.mkDerivation {
+        name = "fourmolu"; # TODO: Necessary to avoid segmentation fault.
+        src = ./.;
+        buildPhase = ''
+          fourmolu --cabal-default-extensions --mode check $(find src -name '*.hs')
+        '';
+        installPhase = ''
+          mkdir $out
+        '';
+        buildInputs = [
+        ];
+        nativeBuildInputs = [
+          haskellPackages.fourmolu
+        ];
+      };
+
+    checks.x86_64-linux.hie-yaml =
+      with import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default ]; };
+      stdenv.mkDerivation {
+        name = "hie-yaml"; # TODO: Necessary to avoid segmentation fault.
+        src = ./.;
+        buildPhase = ''
+          diff --report-identical-files ./hie.yaml <(gen-hie)
+        '';
+        installPhase = ''
+          mkdir $out
+        '';
+        buildInputs = [
+        ];
+        nativeBuildInputs = [
+          haskellPackages.implicit-hie
+        ];
+      };
+
     devShell.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
+      with import nixpkgs { system = "x86_64-linux"; overlays = [ self.overlays.default ]; };
       haskellPackages.shellFor {
         buildInputs = with haskellPackages; [
           cabal-install
+          fourmolu
           ghcid
           haskell-language-server
           hlint
-          hnix
           implicit-hie
           rnix-lsp
         ];
