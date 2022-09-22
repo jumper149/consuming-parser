37bcbd6097a6570122e586d11f1fb2ee66abf166
Felix Springer <felixspringer149@gmail.com>
2022-09-22 16:09:02 +0200

    Format changes.

--- src/Parser/Token/Char.hs
+++ src/Parser/Token/Char.hs
@@ -7,36 +7,37 @@ import Parser qualified as P
 import Parser.Combinators qualified as P
 
 newline :: Monad m => P.ParserT P.Consuming Char () m ()
-newline = P.token P.>>= \case
-  '\n' -> P.pure ()
-  _ -> P.throw $ P.ErrorCustom ()
+newline =
+  P.token P.>>= \case
+    '\n' -> P.pure ()
+    _ -> P.throw $ P.ErrorCustom ()
 
 digit :: Monad m => P.ParserT P.Consuming Char () m Char
 digit = P.do
-    x <- P.token
-    case x of
-      '1' -> P.pure '1'
-      '2' -> P.pure '2'
-      '3' -> P.pure '3'
-      '4' -> P.pure '4'
-      '5' -> P.pure '5'
-      '6' -> P.pure '6'
-      '7' -> P.pure '7'
-      '8' -> P.pure '8'
-      '9' -> P.pure '9'
-      '0' -> P.pure '0'
-      _ -> P.throw $ P.ErrorCustom ()
+  x <- P.token
+  case x of
+    '1' -> P.pure '1'
+    '2' -> P.pure '2'
+    '3' -> P.pure '3'
+    '4' -> P.pure '4'
+    '5' -> P.pure '5'
+    '6' -> P.pure '6'
+    '7' -> P.pure '7'
+    '8' -> P.pure '8'
+    '9' -> P.pure '9'
+    '0' -> P.pure '0'
+    _ -> P.throw $ P.ErrorCustom ()
 
 number :: Monad m => P.ParserT P.Consuming Char () m String
 number = (NonEmpty.toList P.<$> P.some digit) P.<* P.end
 
 floating :: Monad m => P.ParserT P.Consuming Char () m String
 floating = P.do
-    wholes <- NonEmpty.toList P.<$> P.some digit
-    P.terminal '.'
-    fractions <- NonEmpty.toList P.<$> P.some digit
-    P.end
-    P.pure $ wholes ++ "." ++ fractions
+  wholes <- NonEmpty.toList P.<$> P.some digit
+  P.terminal '.'
+  fractions <- NonEmpty.toList P.<$> P.some digit
+  P.end
+  P.pure $ wholes ++ "." ++ fractions
 
 numberParser :: Monad m => P.ParserT P.Consuming Char () m (Either String String)
 numberParser = (Left P.<$> number) P.<|> (Right P.<$> floating)
