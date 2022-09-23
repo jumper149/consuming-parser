84e5448d9006a290d35938cdf2c3479ac72e6a48
Felix Springer <felixspringer149@gmail.com>
2022-09-23 21:08:22 +0200

    Define `Error` before `Trace`.

--- src/Parser/Core/Error.hs
+++ src/Parser/Core/Error.hs
@@ -5,6 +5,17 @@ import Parser.Core.Index
 import Data.Kind
 import GHC.Generics
 
+type Error :: Type -> Type -- TODO: This is redundant.
+data Error :: Type -> Type where
+  ErrorCustom :: e -> Error e
+  ErrorInputEmpty :: Error e
+  ErrorInputLeft :: Error e
+  ErrorSatisfy :: Error e
+  ErrorUnexpectedToken :: Error e
+  ErrorEqual :: Error e
+  ErrorOneOf :: Error e
+  deriving stock (Prelude.Eq, Generic, Prelude.Ord, Prelude.Read, Prelude.Show)
+
 type Trace :: Type -> Type -- TODO: This is redundant.
 data Trace :: Type -> Type where
   TracePoint :: Index -> Error e -> Trace e
@@ -16,14 +27,3 @@ instance Semigroup (Trace e) where
 
 instance Monoid (Trace e) where
   mempty = error "This instance is only implemented to be able to derive an `Alternative` instance."
-
-type Error :: Type -> Type -- TODO: This is redundant.
-data Error :: Type -> Type where
-  ErrorCustom :: e -> Error e
-  ErrorInputEmpty :: Error e
-  ErrorInputLeft :: Error e
-  ErrorSatisfy :: Error e
-  ErrorUnexpectedToken :: Error e
-  ErrorEqual :: Error e
-  ErrorOneOf :: Error e
-  deriving stock (Prelude.Eq, Generic, Prelude.Ord, Prelude.Read, Prelude.Show)
