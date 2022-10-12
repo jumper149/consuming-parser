21babc0dbf5ed7a71057b706d0c799d27c7cd6fd
Felix Springer <felixspringer149@gmail.com>
2022-10-12 19:47:46 +0200

    Add new module `Parser.TypeError`.

---
+++ src/Parser/TypeError.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+{-# OPTIONS_GHC -Wno-missing-methods #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-}
+
+module Parser.TypeError where
+
+import Parser.Core qualified as P
+
+import Control.Applicative
+import Control.Monad.Trans.Class
+import GHC.TypeLits
+import Prelude
+
+type UnsupportedParametricFunctor :: ErrorMessage -> ErrorMessage
+type UnsupportedParametricFunctor shownType =
+  Text "Parametric Functors such as ‘" :<>: ShowType P.ParserT :<>: Text "’ are not supported by the class"
+    :$$: Text "  ‘" :<>: shownType :<>: Text "’"
+    :$$: Text "Try to use the specialised primitives"
+
+instance TypeError (UnsupportedParametricFunctor (ShowType Functor)) => Functor (P.ParserT c t e m)
+instance TypeError (UnsupportedParametricFunctor (ShowType Applicative)) => Applicative (P.ParserT c t e m)
+instance TypeError (UnsupportedParametricFunctor (ShowType Monad)) => Monad (P.ParserT c t e m)
+instance TypeError (UnsupportedParametricFunctor (ShowType MonadTrans)) => MonadTrans (P.ParserT c t e)
+instance TypeError (UnsupportedParametricFunctor (ShowType Alternative)) => Alternative (P.ParserT c t e m)
