631c2fbf5b1b39583421f37866a1e660c99271c3
Felix Springer <felixspringer149@gmail.com>
2022-10-22 11:48:28 +0200

    Move wrapper function out of flake.

---
+++ nix/haskell/lib.nix
@@ -0,0 +1,22 @@
+{ lib, haskellLib }: {
+  cabal2Increment =
+    { regularPackage, previousIncrement ? null }:
+      (haskellLib.overrideCabal regularPackage
+        (drv: {
+          preBuild = lib.optionalString (previousIncrement != null) ''
+            mkdir -p dist/build
+            tar xzf ${previousIncrement}/dist.tar.gz -C dist/build
+          '';
+          postInstall = ''
+            mkdir $incremental
+            tar czf $incremental/dist.tar.gz -C dist/build --mtime='1970-01-01T00:00:00Z' .
+          '';
+          preFixup = ''
+            # Don't try to strip incremental build outputs
+            outputs=(${"\\" + "\${"}outputs[@]/incremental})
+          '';
+        })
+      ).overrideAttrs (finalAttrs: previousAttrs: {
+        outputs = previousAttrs.outputs ++ ["incremental"];
+      });
+}
